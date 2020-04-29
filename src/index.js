501cc67b1fe8458be1bae7cc5dd6c3d1e414b8ac
unknown <rune.escape888@gmail.com>
2020-04-29 18:21:36 -0400

    updated

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
 const db = require('./dataBase.js');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 const { refreshAccessToken } = require('./middleware/auth.js');
@@ -11,6 +12,7 @@ const  { isAuth } = require('./middleware/auth.js');
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 //routes
 app.use('/', require('./routes/user'))
