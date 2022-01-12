c63b4ddb2b2690d6fbf0ee2f04c419613caa6c6d
Yordinson Polar <yordinsonpolar@gmail.com>
2022-01-12 10:22:30 -0400

    fixed cors

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const { isAuth } = require('./middleware/auth.js');
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser()); 
-app.use(cors());
+app.use(cors({ origin: '*'}));
 
 //routes
 app.use('/', require('./routes/user'))
